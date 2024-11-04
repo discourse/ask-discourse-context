bd3139e027b18e120e09c5d2843de44d1b0e4be8
Jarek Radosz <jarek@cvx.dev>
2024-11-04 17:49:46 +0100

    DEV: Update linting (#1)

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-# name: discourse-ai-stream-id
+# name: ask-discourse-context
 # about: Adds AI Stream conversation unique ID to user cards
 # version: 0.1
 # authors: Sam
-# url: https://github.com/discourse/discourse-ai-stream-id
+# url: https://github.com/discourse/ask-discourse-context
 
 # The ask theme can then use this information to render links to sites
 
@@ -11,8 +11,6 @@ after_initialize do
   add_to_serializer(
     :user_card,
     :ai_stream_conversation_unique_id,
-    include_condition: -> do
-      scope&.user&.admin
-    end
+    include_condition: -> { scope&.user&.admin },
   ) { object.custom_fields["ai-stream-conversation-unique-id"] }
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -4,19 +4,19 @@ describe UsersController do
   fab!(:user) do
     user = Fabricate(:user)
     user.custom_fields["ai-stream-conversation-unique-id"] = "test"
-    user.save
+    user.save!
     user
   end
-  fab!(:admin) { Fabricate(:admin) }
+  fab!(:admin)
 
   describe "#index" do
-    it "should not serialize data for regural users" do
+    it "does not serialize data for regular users" do
       sign_in(user)
       get "/u/#{user.username_lower}/card.json"
       expect(response.parsed_body["user"]["ai_stream_conversation_unique_id"]).to eq(nil)
     end
 
-    it "should serialize data for admin users" do
+    it "serializes data for admin users" do
       sign_in(admin)
       get "/u/#{user.username_lower}/card.json"
       expect(response.parsed_body["user"]["ai_stream_conversation_unique_id"]).to eq("test")
