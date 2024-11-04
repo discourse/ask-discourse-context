c51f93463200a78942e4b04b7c117997331b48a1
Sam Saffron <sam.saffron@gmail.com>
2024-11-04 14:32:12 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Tiny plugin that adds context about the source of a user created via Discourse AI
+to the payload on user cards.
---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# name: discourse-ai-stream-id
+# about: Adds AI Stream conversation unique ID to user cards
+# version: 0.1
+# authors: Sam
+# url: https://github.com/discourse/discourse-ai-stream-id
+
+# The ask theme can then use this information to render links to sites
+
+after_initialize do
+  add_to_serializer(:user_card, :ai_stream_conversation_unique_id) do
+    object.custom_fields["ai-stream-conversation-unique-id"]
+  end
+end
